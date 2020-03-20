de2fab901a854d77a5c076a548c05768e9aba9e0
fatmaosama429 <fatma.abdelrahim99@edu-st.cu.edu.eg>
2020-03-20 15:49:25 +0200

    error is solved

--- queue_lec3/main.cpp
+++ queue_lec3/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include<c_queue.h>
+#include "c_queue.h"
+#include "c_queue.cpp"
 
 int main()
 {
  c_queue q1;
  q1.enqueue(3);
- std::cout<<q1.dequeue();
+ std::cout<<q1.dequeue()<<std::endl;
     return 0;
 }
